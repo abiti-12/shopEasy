c58c3cf72f753b31a3b2147697ab2deca005cdcb
abiti-12 <mintesnottamiru78@gmail.com>
2026-07-07 10:24:05 +0300

    Use MongoDB environment variable

--- api/config/db.js
+++ api/config/db.js
@@ -1,23 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-
     try {
-
-        await mongoose.connect("mongodb://127.0.0.1:27017/ecommerce");
+        await mongoose.connect(process.env.MONGODB_URI);
 
         console.log("✅ MongoDB Connected");
-
     } catch (error) {
-
         console.error("❌ Database Connection Failed");
-
         console.error(error.message);
-
         process.exit(1);
-
     }
-
 };
 
 module.exports = connectDB;
